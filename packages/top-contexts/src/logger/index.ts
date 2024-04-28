f17d682c42fe8d13668c29701b75e8c95a18433c
Nicu Tofan <nicu.tofan@gmail.com>
2024-04-28 11:40:29 +0300

    Wrong package

--- packages/top-contexts/src/logger/index.ts
+++
@@ -1,9 +0,0 @@
-export type { LoggerContextData } from './context';
-export {
-    LoggerContext,
-    LoggerProvider,
-    useLogger
-} from './context';
-
-export type { LoggerControllerProps } from './controller';
-export { LoggerController } from './controller';
--- packages/top-contexts/src/urls/index.ts
+++
@@ -1,9 +0,0 @@
-export type { AppUrlsContextData } from './context';
-export {
-    AppUrlsContext,
-    AppUrlsProvider,
-    useAppUrls
-} from './context';
-
-export type { AppUrlsControllerProps } from './controller';
-export { AppUrlsController } from './controller';
